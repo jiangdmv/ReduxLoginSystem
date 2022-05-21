0b0378caafedb0efafeef4202cc13e3558440634
jiangdmv <jiangdmv@gmail.com>
2022-05-21 17:13:20 -0400

    5/21

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import { FaSignInAlt, FaSignOutAlt, FaUber, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">NewSite</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = () => {};
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            ></input>
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            ></input>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Login;
